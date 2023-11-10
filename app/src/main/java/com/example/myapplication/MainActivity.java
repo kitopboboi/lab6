429277a4fb4aa207e01ee377b732d3f1bea166f2
kitopboboi <t110360134@ntut.org.tw>
2023-11-10 15:04:14 +0800

    first commti

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,88 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    class Data{
+        int photo;
+        String name;
+    }
+
+    public class MyAdapter extends BaseAdapter {
+        private MainActivity.Data[]data;
+        private int view;
+
+        public MyAdapter (MainActivity.Data[] data, int view){
+            this.data = data;
+            this.view = view;
+        }
+
+        @Override
+        public int getCount() {return data.length; }
+        @Override
+        public Object getItem(int position) { return data[position]; }
+
+        @Override
+        public long getItemId(int position) { return 0; }
+
+        @Override
+        public View getView(int position , View convertView, ViewGroup parent) {
+            convertView= getLayoutInflater().inflate(view, parent, false);
+            TextView name= convertView.findViewById(R.id.name);
+            name.setText(data[position].name);
+            ImageView imageView= convertView.findViewById(R.id.imageView);
+            imageView.setImageResource(data[position].photo);
+
+            return convertView;
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        String[] transNameArray = new String[]{"Train", "Bus", "Car", "Airplane"};
+        int[] transPhotoIdArray = new int[]{R.drawable.trans1, R.drawable.trans2,
+                R.drawable.trans3, R.drawable.trans4, R.drawable.trans5, R.drawable.trans6};
+
+        Data[] transData = new Data[transNameArray.length];
+        for (int i = 0; i < transData.length; i++){
+            transData[i]=new Data();
+            transData[i].name = transNameArray[i];
+            transData[i].photo = transPhotoIdArray[i];
+        }
+
+        MyAdapter transAdapter = new MyAdapter(transData, R.layout.trans_list);
+        Spinner spinner = findViewById(R.id.spinner);
+        spinner.setAdapter(transAdapter);
+
+        String[] cubeeNameArray = new String[]{"哭哭", "發抖",
+                "再見", "生氣",
+                "smth1", "smth2",
+                "很棒", "你好",
+                "smth3", "大笑"};
+
+        int[] cubeePhotoIdArray = new int[]{R.drawable.cubee1, R.drawable.cubee2
+                R.drawable.cubee3, R.drawable.cubee4,
+                R.drawable.cubee5, R.drawable.cubee6,
+                R.drawable.cubee7, R.drawable.cubee8,
+                R.drawable.cubee9, R.drawable.cubee10,};
+
+        Data[] cubeeData = new Data[cubeeNameArray.length];
+        for (int i = 0; i < cubeeData.length; i++) {
+            cubeeData[i] = new Data();
+            cubeeData[i].name = cubeeNameArray[i];
+            cubeeData[i].photo = cubeePhotoIdArray[i];
+        }
+        MyAdapter cubeeAdapter = new
+    }
+}
